5d7e770a415f943bd778ec590669edddf5db9e01
Adityaa-Sharma <mailmeifyoucan7@gmail.com>
2025-11-09 23:08:20 +0530

    few bug fixes in the coded, and also added the output ss

--- pointers/kernels/vector_add.cu
+++ pointers/kernels/vector_add.cu
@@ -4,19 +4,19 @@
 using namespace std;
 
 // size of the each vector 
-int N =10000000;
+int N =100000000;
 int blockSize = 256;
 
 // normal cpu execution code
 
-void vector_addition_cpu( int* h_a, int* h_b, int* h_c, int size){
+void vector_addition_cpu( float* h_a, float* h_b, float* h_c, int size){
     for(int i =0;i<size;i++){
         h_c[i]=h_a[i]+h_b[i];
     }
 }
 
 // gpu kernel code
-__global__ void(int*d_a , int*d_b, int*d_c,int size){
+__global__ void vector_addition_kernel(float*d_a , float*d_b, float*d_c,int size){
     int i =blockIdx.x*blockDim.x +threadIdx.x;
     if(i<size){
         d_c[i]=d_a[i]+d_b[i];
@@ -25,22 +25,22 @@ __global__ void(int*d_a , int*d_b, int*d_c,int size){
 
 
 // functioon to initialize the vectors
-void vector_init(int* vect , int size){
+void vector_init(float* vect , float size){
     for( int i =0; i<size; i++){
         vect[i]=(float)(rand()/RAND_MAX);
     }
 }
 
-void get_time(){
+double get_time(){
     struct timespec ts;
     clock_gettime(CLOCK_REALTIME, &ts);
-    return ts.tv_sec + ts.tv_nsec * 1e9;
+    return ts.tv_sec + ts.tv_nsec * 1e-9;
 }
 
 
 int main(){
-    int *h_a, *h_b, *h_output_cpu, *h_output_gpu;
-    int *d_a, *d_b, *d_c;
+    float *h_a, *h_b, *h_output_cpu, *h_output_gpu;
+    float *d_a, *d_b, *d_c;
     
 
     // memory allocation on host
@@ -56,13 +56,13 @@ int main(){
     vector_init(h_b,N);
 
     // memory allocation on device
-    cudamalloc(&h_a, bytes);
-    cudamalloc(&h_b, bytes);
-    cudamalloc(&h_c, bytes);
+    cudaMalloc(&d_a, bytes);
+    cudaMalloc(&d_b, bytes);
+    cudaMalloc(&d_c, bytes);
 
     // copy data from host to device
-    cudamemcpy(d_a, h_a, bytes, cudaMemcpyHostToDevice);
-    cudamemcpy(d_b, h_b, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_a, h_a, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, bytes, cudaMemcpyHostToDevice);
 
     // block size and grid size
     int num_blocks=(N+blockSize-1)/blockSize;
@@ -70,37 +70,38 @@ int main(){
     printf("warming up \n");
     for (int i =0;i<5;i++){
         vector_addition_kernel<<<num_blocks, blockSize>>>(d_a, d_b, d_c, N);
-        cudadevicesynchronize();
+        cudaDeviceSynchronize();
     }
 
     // Benchmarking
-    print ("Starting CPU computation \n");
-    double cpu_total_time=get_time();
-    for (int i=0; i<100; i++){
+    printf("Starting CPU computation \n");
+    double cpu_total_time=0.0;
+    for (int i=0; i<1000; i++){
         double iter_time_start=get_time();
         vector_addition_cpu(h_a, h_b, h_output_cpu, N);
         double iter_time_end=get_time();
         cpu_total_time += iter_time_end - iter_time_start;
     }
-    double cpu_avg_time=cpu_total_time/100.0;
-    printf("Average CPU time: %f ms \n", cpu_avg_time/1e6);
+    double cpu_avg_time=cpu_total_time/1000.0;
+    printf("Average CPU time: %f s \n", cpu_avg_time);
 
 
-    print ("Starting GPU computation \n");
-    double gpu_total_time=get_time();
+    printf("Starting GPU computation \n");
+    double gpu_total_time=0.0;
     for (int i=0; i<100; i++){
         double iter_time_start=get_time();
         vector_addition_kernel<<<num_blocks, blockSize>>>(d_a, d_b, d_c, N);
-        cudadevicesynchronize();
+        cudaDeviceSynchronize();
         double iter_time_end=get_time();
         gpu_total_time += iter_time_end - iter_time_start;
     }
-    printf("Average GPU time: %f ms \n", gpu_avg_time/1e6);
+    double gpu_avg_time=gpu_total_time/100.0;
+    printf("Average GPU time: %f s \n", gpu_avg_time);
 
     // verify the result
-    cudamemcpy(h_output_gpu, d_c, bytes, cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_output_gpu, d_c, bytes, cudaMemcpyDeviceToHost);
     bool match = true;
-    for (int i -0; i< N; i++){
+    for (int i=0; i< N; i++){
         if (fabs(h_output_cpu[i] - h_output_gpu[i]) > 1e-5){
             match = false;
             printf("Results do not match! CPU: %f, GPU: %f \n", h_output_cpu[i], h_output_gpu[i]);
@@ -114,9 +115,9 @@ int main(){
     free(h_b);
     free(h_output_cpu);
     free(h_output_gpu);
-    cudafree(d_a);
-    cudafree(d_b);
-    cudafree(d_c);
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
     return 0;
 
 }
